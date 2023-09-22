575c195ad4eae0220e0bee91173bc36aa19fe13a
Nguyễn Lê Công Duy <119640974+Zui1506@users.noreply.github.com>
2023-09-22 20:11:12 +0700

    Create main.cpp

---
+++ Practice/Exercise_2/main.cpp
@@ -0,0 +1,13 @@
+#include"date.h"
+#include"Employee.h"
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    employee a;
+    a.get_employ();    // Nhap thong tin nhan vien
+    a.put_employ();    // Xuat thong tin nhan vien
+    return 0;
+}
