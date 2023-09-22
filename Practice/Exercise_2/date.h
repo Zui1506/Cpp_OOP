a6cf3bf2c7893691f79dfa120d325d10d7fd2377
Nguyễn Lê Công Duy <119640974+Zui1506@users.noreply.github.com>
2023-09-22 20:11:50 +0700

    Create date.h

---
+++ Practice/Exercise_2/date.h
@@ -0,0 +1,15 @@
+#include<iostream>
+#pragma once
+using namespace std;
+
+class Dates
+{
+    private:
+        int day;
+        int month;
+        int year;
+    public:
+
+        void get_date();
+        void show_date();
+};
