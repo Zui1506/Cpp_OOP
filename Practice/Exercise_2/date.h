aa75873bdc1fcd73e4b36c2f5e82344376765114
Nguyễn Lê Công Duy <119640974+Zui1506@users.noreply.github.com>
2023-09-28 12:51:02 +0700

    Update date.h

--- Practice/Exercise_2/date.h
+++ Practice/Exercise_2/date.h
@@ -1,15 +1,19 @@
-#include<iostream>
-#pragma once
-using namespace std;
+#ifndef DATE_H
+#define DATE_H
 
-class Dates
+
+class date
 {
     private:
         int day;
         int month;
         int year;
     public:
-
+        date();
+        virtual ~date();
         void get_date();
         void show_date();
+
 };
+
+#endif // DATE_H
